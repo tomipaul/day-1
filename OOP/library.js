3e16083b186f4852c8c0d38d92edc0dc6867830f
Oluwatomi Akande <tomipaul95@gmail.com>
2017-01-09 16:22:17 +0100

    An implementation of a simple library system using OOP. Five classes were defined; library, book, universityStudent, undergraduateStudent and postgraduateStudent. undergraduateStudent and postgraduateStudent extends the universityStudent class

---
+++ OOP/library.js
@@ -0,0 +1,81 @@
+class universityStudent {
+	constructor(name, matricNo) {
+		this.name = name;
+		this.matricNo = matricNo;
+		this.borrowedBooks = {};
+		this.borrowedBooksCount=0;
+	}
+
+	returnToLibrary(libraryInstance, bookInstance) {
+		if (bookInstance.libraryRefNo in this.borrowedBooks) {
+			delete this.borrowedBooks[bookInstance.libraryRefNo];
+			this.borrowedBooksCount-=1;
+			libraryInstance.addBook(bookInstance, bookInstance.libraryRefNo);
+		}
+	}
+}
+
+class undergraduateStudent extends universityStudent {
+	constructor(name, matricNo) {
+		super(name, matricNo);
+	}
+
+	borrowFromLibrary(libraryInstance, bookInstance) {
+		if (libraryInstance.type = "research") {
+			return "Hey pal, this is a research library. Research libraries are for PG students"
+		}
+		if (this.borrowedBooksCount!=3) {
+			if(bookInstance.libraryRefNo in libraryInstance.books) {
+				this.borrowedBooks[bookInstance.libraryRefNo] = bookInstance;
+				this.borrowedBooksCount+=1;
+				libraryInstance.removeBook(bookInstance)
+		}
+		else {
+			return "You can only borrow 3 books from the library";
+		}
+	}
+}
+
+class postgraduateStudent extends universityStudent {
+	constructor(name, matricNo) {
+		super(name, matricNo);
+	}
+
+	borrowFromLibrary(libraryInstance, bookInstance) {
+		if (this.borrowedBooksCount != 5) {
+			if(bookInstance.libraryRefNo in libraryInstance.books) {
+				this.borrowedBooks[bookInstance.libraryRefNo] = bookInstance;
+				this.borrowedBooksCount+=1;
+				libraryInstance.removeBook(bookInstance);
+			}
+		}
+		else {
+			return "You can only borrow 5 books from the library";
+		}
+	}
+}
+
+class library {
+	constructor(books, type) {
+		this.books = {};
+		this.type = [];
+	}
+
+	addBook(bookInstance, libraryRefNo) {
+		bookInstance.libraryRefNo = libraryRefNo;
+		this.books[libraryRefNo] = bookInstance;
+	}
+
+	removeBook(bookInstance) {
+		delete this.book[bookInstance.libraryRefNo];
+	}
+}
+
+class book {
+	constructor(name, author, category, pageNo) {
+		this.name=name;
+		this.author = author;
+		this.category = category;
+		this.pageNo = pageNo;
+	}
+}
